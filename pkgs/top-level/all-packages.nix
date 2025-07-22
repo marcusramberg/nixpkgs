2a1cef52c0379a47e4a19b148cd8b59353616024
Marcus Ramberg <marcus@means.no>
2025-07-22 18:15:22 +0200

    perl: 5.40.0 -> 5.42.0
    Drop 0.38 and 5.40

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -334,10 +334,7 @@ with pkgs;
     stdenv = clangStdenv;
   };
 
-  cope = callPackage ../by-name/co/cope/package.nix {
-    perl = perl538;
-    perlPackages = perl538Packages;
-  };
+  cope = callPackage ../by-name/co/cope/package.nix { };
 
   coolercontrol = recurseIntoAttrs (callPackage ../applications/system/coolercontrol { });
 
@@ -9953,13 +9950,12 @@ with pkgs;
   ### DEVELOPMENT / PERL MODULES
 
   perlInterpreters = import ../development/interpreters/perl { inherit callPackage; };
-  inherit (perlInterpreters) perl538 perl540;
+  inherit (perlInterpreters) perl5;
 
-  perl538Packages = recurseIntoAttrs perl538.pkgs;
-  perl540Packages = recurseIntoAttrs perl540.pkgs;
+  perl5Packages = recurseIntoAttrs perl5.pkgs;
 
-  perl = perl540;
-  perlPackages = perl540Packages;
+  perl = perl5;
+  perlPackages = perl5Packages;
 
   ack = perlPackages.ack;
 
