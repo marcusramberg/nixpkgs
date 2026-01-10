a7fe0c34a2371b9098bd5fd9b4849d6e7bce2d0a
Marcus Ramberg <marcus@means.no>
2026-01-10 21:21:10 +0100

    perlPackages.PlackMiddlewareDeflater: 0.12 -> 0.14

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -28195,10 +28195,10 @@ with self;
 
   PlackMiddlewareDeflater = buildPerlPackage {
     pname = "Plack-Middleware-Deflater";
-    version = "0.12";
+    version = "0.14";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/K/KA/KAZEBURO/Plack-Middleware-Deflater-0.12.tar.gz";
-      hash = "sha256-KNqV59pMi1WRrEVFCckhds0IQpYM4HT94w+aEHXcwnU=";
+      url = "mirror://cpan/authors/id/M/MI/MIYAGAWA/Plack-Middleware-Deflater-0.14.tar.gz";
+      hash = "sha256-O+YLOD6eAtxfMkCbqIs3zXWEwnXQKDLardZOcxFnhYY=";
     };
     propagatedBuildInputs = [ Plack ];
     buildInputs = [
