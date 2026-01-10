94a04eb3f66976af2b8f5620fd2f9d8c087999c5
Marcus Ramberg <marcus@means.no>
2026-01-10 21:20:37 +0100

    perlPackages.MIMETypes: 2.24 -> 2.30

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -21467,10 +21467,10 @@ with self;
 
   MIMETypes = buildPerlPackage {
     pname = "MIME-Types";
-    version = "2.24";
+    version = "2.30";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/M/MA/MARKOV/MIME-Types-2.24.tar.gz";
-      hash = "sha256-Yp42HyKyIL5QwtpzVOI8BFF1dwmgPCWiLzFg7blMtl8=";
+      url = "mirror://cpan/authors/id/M/MA/MARKOV/MIME-Types-2.30.tar.gz";
+      hash = "sha256-8xsWZr30ILS2XDc84BKe40ndJLq0zRbH8Btpj+RQvm8=";
     };
     meta = {
       description = "Definition of MIME types";
