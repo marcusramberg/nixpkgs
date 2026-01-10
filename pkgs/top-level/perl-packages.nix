965379afd84ab4befee44412a12d921a801dc1c9
Marcus Ramberg <marcus@means.no>
2026-01-10 21:20:05 +0100

    perlPackages.DateTimeTimeZone: 2.60 -> 2.66

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -9301,10 +9301,10 @@ with self;
 
   DateTimeTimeZone = buildPerlPackage {
     pname = "DateTime-TimeZone";
-    version = "2.60";
+    version = "2.66";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/D/DR/DROLSKY/DateTime-TimeZone-2.60.tar.gz";
-      hash = "sha256-8EYNN5MjkFtXm+1E4UEjejN9wl3Sa2qwxgrCuAYpMj0=";
+      url = "mirror://cpan/authors/id/D/DR/DROLSKY/DateTime-TimeZone-2.66.tar.gz";
+      hash = "sha256-b71hstXXVjqOc4NBw3kODUMC3/1Kh5ucJogS/kS1TcE=";
     };
     buildInputs = [
       TestFatal
