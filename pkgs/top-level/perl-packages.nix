6b8489f43229023ec16605709f19568f2154f7bd
Marcus Ramberg <marcus@means.no>
2026-01-10 21:20:25 +0100

    perlPackages.HTMLRewriteAttributes: 0.05 -> 0.06

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -16189,7 +16189,7 @@ with self;
 
   HTMLRewriteAttributes = buildPerlPackage {
     pname = "HTML-RewriteAttributes";
-    version = "0.05";
+    version = "0.06";
     src = fetchurl {
       url = "mirror://cpan/authors/id/B/BP/BPS/HTML-RewriteAttributes-0.06.tar.gz";
       hash = "sha256-vGQgAmEUL5pffgeG3FqySmMwvBm9Hj6btLbXwxYhtyI=";
